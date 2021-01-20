8d209dd1bb23beb4e31144ced051d80942faf506
Renan Lima <renanlimabl@gmail.com>
2021-01-20 09:55:01 -0300

    adiciona dependencias de estilos

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core';
 
 import Login from './pages/Login';
 import Main from './pages/Main';
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route component={Main} />
-    </Switch>
-  </BrowserRouter>
+  <>
+    <CssBaseline />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route component={Main} />
+      </Switch>
+    </BrowserRouter>
+  </>
 );
 
 export default App;
