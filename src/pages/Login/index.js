f02c6802bc49a82a53f1184fd58653d5c1154340
Renan Lima <renanlimabl@gmail.com>
2021-01-21 01:32:59 -0300

    add estilos para pagina de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Button, Grid } from '@material-ui/core';
+import { ReactComponent as MainLogo } from './logo-react-zzaria.svg';
 
 const Login = () => (
-  <h1>Login</h1>
+  <Container>
+    <Grid container justify="center" spacing={10}>
+      <Grid item>
+        <Logo style={{ width: '100%' }} />
+      </Grid>
+      <Grid item container justify="center" xs={12}>
+        <GithubButton>Entrar com Github</GithubButton>
+      </Grid>
+    </Grid>
+  </Container>
 );
+
+const Container = styled.div`
+  padding: 40px;
+`;
+
+const Logo = styled(MainLogo)`
+  width: 100%;
+`;
+
+const GithubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true,
+})`
+  && {
+    font-size: 20px;
+    padding: 15px;
+    text-transform: none;
+    max-width: 480px;
+  }
+`;
 
 export default Login;
