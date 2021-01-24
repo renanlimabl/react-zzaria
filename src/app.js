89c8fa3e1ba91fd8a953697e63681dcbbea06fc8
Renan Lima <renanlimabl@gmail.com>
2021-01-24 06:50:10 -0300

    refactor: components do app dentro do browser router e o context api envendendo todo app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { CssBaseline, LinearProgress } from '@material-ui/core';
-
+import React, { Suspense, lazy } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { LinearProgress } from '@material-ui/core';
 /**
  * Com lazy loading, o import fica mais dinâmico, porque só é importado aquela página
  * que foi requesitada, não é importada todas de uma vez como se fosse o import normal
@@ -9,18 +8,15 @@ import { CssBaseline, LinearProgress } from '@material-ui/core';
 const Main = lazy(() => import('./pages/Main'));
 const Login = lazy(() => import('./pages/Login'));
 
-const App = () => (
-  <>
-    <CssBaseline />
-    <BrowserRouter>
-      <Suspense fallback={<LinearProgress />}>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route component={Main} />
-        </Switch>
-      </Suspense>
-    </BrowserRouter>
-  </>
-);
+function App() {
+  return (
+    <Suspense fallback={<LinearProgress />}>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route component={Main} />
+      </Switch>
+    </Suspense>
+  );
+}
 
 export default App;
