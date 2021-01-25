57c5fe02b6aea79e566315760eddbd8f9543dfb8
Renan Lima <renanlimabl@gmail.com>
2021-01-25 08:21:51 -0300

    estiliza header

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import styled from 'styled-components';
 import {
   AppBar,
   Toolbar,
   IconButton,
+  Typography,
+  Menu,
+  MenuItem,
 } from '@material-ui/core';
 
 import { AccountCircle } from '@material-ui/icons';
@@ -12,12 +16,36 @@ import { ReactComponent as MainLogo } from '../Login/logo-react-zzaria.svg';
 const Main = () => (
   <AppBar>
     <Toolbar>
-      <MainLogo />
+      <LogoContainer>
+        <Logo />
+      </LogoContainer>
+      <Typography>
+        Olá João =)
+      </Typography>
       <IconButton color="inherit">
         <AccountCircle />
       </IconButton>
+      <Menu open={false}>
+        <MenuItem>Sair</MenuItem>
+      </Menu>
     </Toolbar>
   </AppBar>
 );
 
+const LogoContainer = styled.div`
+  flex-grow: 1;
+`;
+
+const Logo = styled(MainLogo)`
+  width: 200px;
+  height: 50px;
+
+  & path {
+    fill: #fff;
+  }
+
+  & line {
+    stroke: #fff;
+  }
+`;
 export default Main;
